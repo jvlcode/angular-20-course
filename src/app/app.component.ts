1d1b574f91a305efa42564ea149fd6c6a6ad1db0
JVLcode <81806680+jvlcode@users.noreply.github.com>
2025-05-27 16:13:33 +0530

    Reactive Forms Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PostListComponent } from './components/post-list/post-list.component';
+import { ReactiveFormsExampleComponent } from './components/reactive-forms-example/reactive-forms-example.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PostListComponent],
+  imports: [RouterOutlet, PostListComponent, ReactiveFormsExampleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
