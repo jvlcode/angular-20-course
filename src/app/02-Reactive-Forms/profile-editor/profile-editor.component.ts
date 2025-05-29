e06ee761bfdfa6204e61f9d93ef2b3ce13529688
JVLcode <81806680+jvlcode@users.noreply.github.com>
2025-05-29 21:10:35 +0530

    02 Associate the FormGroup model and view

--- src/app/02-Reactive-Forms/profile-editor/profile-editor.component.ts
+++ src/app/02-Reactive-Forms/profile-editor/profile-editor.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-editor',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './profile-editor.component.html',
   styleUrl: './profile-editor.component.css'
 })
 export class ProfileEditorComponent {
-
+ profileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+  });
 }
