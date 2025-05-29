3396bb948b35a71207b002923a964ae5a4337235
JVLcode <81806680+jvlcode@users.noreply.github.com>
2025-05-29 21:01:46 +0530

    02-Reactive-Forms: Adding a basic form control
    import the ReactiveFormsModule
    Declare a FormControl instance
    Register the control in the template
    Displaying a form control value

--- src/app/02-Reactive-Forms/name-editor/name-editor.component.ts
+++ src/app/02-Reactive-Forms/name-editor/name-editor.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-name-editor',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './name-editor.component.html',
   styleUrl: './name-editor.component.css'
 })
 export class NameEditorComponent {
-
+  name = new FormControl('');
 }
