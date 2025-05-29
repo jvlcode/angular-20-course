807eba53ef39090d08d117bc4225e9be6be11b00
JVLcode <81806680+jvlcode@users.noreply.github.com>
2025-05-29 21:06:10 +0530

    02-Reactive-Forms: Grouping form controls
    Generate a ProfileEditor component

---
+++ src/app/02-Reactive-Forms/profile-editor/profile-editor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-editor',
+  imports: [],
+  templateUrl: './profile-editor.component.html',
+  styleUrl: './profile-editor.component.css'
+})
+export class ProfileEditorComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PostComponent } from './01-HttpClient/post/post.component';
 import { NameEditorComponent } from './02-Reactive-Forms/name-editor/name-editor.component';
+import { ProfileEditorComponent } from './02-Reactive-Forms/profile-editor/profile-editor.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NameEditorComponent],
+  imports: [RouterOutlet, NameEditorComponent, ProfileEditorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
