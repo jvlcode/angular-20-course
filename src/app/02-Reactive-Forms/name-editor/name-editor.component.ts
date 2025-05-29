596e622ee962e2145fc10a5c199d1b80a913b782
JVLcode <81806680+jvlcode@users.noreply.github.com>
2025-05-29 20:58:16 +0530

    02-Reactive-Forms: create name editor component

---
+++ src/app/02-Reactive-Forms/name-editor/name-editor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-name-editor',
+  imports: [],
+  templateUrl: './name-editor.component.html',
+  styleUrl: './name-editor.component.css'
+})
+export class NameEditorComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PostComponent } from './01-HttpClient/post/post.component';
+import { NameEditorComponent } from './02-Reactive-Forms/name-editor/name-editor.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PostComponent],
+  imports: [RouterOutlet, NameEditorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
