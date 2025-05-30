71ccbe4de2784da71839673a68d3124f0530d5e3
JVLcode <81806680+jvlcode@users.noreply.github.com>
2025-05-30 15:51:46 +0530

    02 Signals - Computed Signals - Effects

--- src/app/03-Signals/counter/counter.component.ts
+++ src/app/03-Signals/counter/counter.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, computed, Signal, signal, effect } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   imports: [],
   templateUrl: './counter.component.html',
-  styleUrl: './counter.component.css'
 })
 export class CounterComponent {
+  count = signal(0);
+  doubleCount: Signal<number> = computed(() => this.count() * 2);
+  constructor() {
+    effect(() => {
+      console.log(`The current count is: ${this.count()}`);
+    });
+  }
+
+  increment() {
+    this.count.set(this.count() + 1);
+  }
+
 
 }
