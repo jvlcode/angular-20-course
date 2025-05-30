dcd0c854b3c8e1ca298d36896425a4cd79a0ee4c
JVLcode <81806680+jvlcode@users.noreply.github.com>
2025-05-30 18:14:38 +0530

    feat: ReactForms

---
+++ src/app/02-Reactive-Forms/profile-editor/profile-editor.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-profile-editor',
+  imports: [ReactiveFormsModule],
+  templateUrl: './profile-editor.component.html',
+  styleUrl: './profile-editor.component.css'
+})
+export class ProfileEditorComponent {
+ 
+
+  profileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName : new FormControl('')
+  })
+
+  onSubmit() {
+    console.log(this.profileForm.value)
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PostComponent } from './01-HttpClient/post/post.component';
+import { NameEditorComponent } from './02-Reactive-Forms/name-editor/name-editor.component';
+import { ProfileEditorComponent } from './02-Reactive-Forms/profile-editor/profile-editor.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PostComponent],
+  imports: [RouterOutlet, PostComponent, NameEditorComponent, ProfileEditorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
